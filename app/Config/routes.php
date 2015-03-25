5f5bcfca0b65045d80422d74e4b94fa30d09e560
Nick Baker <nick@webtechnick.com>
2015-03-24 23:44:41 -0600

    Adding routes

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -17,12 +17,22 @@
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
 	Router::connect('/', array('controller' => 'pages', 'action' => 'home'));
-
+	Router::connect('/admin',array('admin' => true, 'plugin' => null, 'controller'=>'codes','action'=>'index'));
+	Router::connect('/login', array('controller' => 'users', 'action' => 'login'));
+	Router::connect('/logout', array('controller' => 'users', 'action' => 'logout'));
+  Router::connect('/register', array('controller' => 'users', 'action' => 'register'));
+  Router::connect('/account', array('controller' => 'users', 'action' => 'account'));
+  //Redirect admin login to user login
+	Router::connect('/admin/users/login', array('admin' => false, 'controller' => 'users', 'action' => 'login'));
 /**
  * Load all plugin routes. See the CakePlugin documentation on
  * how to customize the loading of plugin routes.
  */
 	CakePlugin::routes();
+
+	//combine edit and add
+	Router::connect('/admin/:controller/add', array('admin' => true, 'controller' => ':controller', 'action' => 'edit'));
+	Router::connect('/:controller/add', array('controller' => ':controller', 'action' => 'edit'));
 
 /**
  * Load the CakePHP default routes. Only remove this if you do not want to use
